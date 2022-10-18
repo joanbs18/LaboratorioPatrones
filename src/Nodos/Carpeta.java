d43be0b54710cf48bf83d8591174f233821fff7c
Joseph Carazo <jocarazope@est.utn.ac.cr>
2022-10-18 11:56:15 -0600

    Se crea el verificar en Carpeta

--- src/Nodos/Carpeta.java
+++ src/Nodos/Carpeta.java
@@ -5,36 +5,53 @@
 package Nodos;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Francisco
  */
 public class Carpeta extends Nodo {
-    public ArrayList <Nodo> contenedor=new ArrayList();
 
-    public Carpeta(String nombre,int tamanio) {
+    public static ArrayList<Nodo> contenedor = new ArrayList();
+
+    public Carpeta(String nombre, int tamanio) {
         this.setTipoN(Nodo.CARPETA);
         this.setNombre(nombre);
         this.setTamanio(tamanio);
     }
+
+    public static boolean verificar(String nombre) {
+        for (int i = 0; i < contenedor.size(); i++) {
+            if (contenedor.get(i).getNombre().equals(nombre)) {
+                JOptionPane.showMessageDialog(null, "El nombre de la carpeta ya existe. Por favor, nombre distinto a la carpeta.", "Se ha producido un error.", JOptionPane.ERROR_MESSAGE);
+                nombre = JOptionPane.showInputDialog(null, "Escriba un nombre para su carpeta", "CARPETA", JOptionPane.QUESTION_MESSAGE);
+                return true;
+            }
+        }
+        return false;
+    }
+
     //se crea carpeta por defecto
     public Carpeta() {
         this.setTipoN(Nodo.CARPETA);
         this.setNombre("Nueva Carpeta");
         this.setTamanio(0);
     }
-    
+
     //para agregar dentro de la carpeta ya sea otra carpeta 
-    public void agregar(Nodo tipoNodo){
+    public void agregar(Nodo tipoNodo) {
         contenedor.add(tipoNodo);
     }
-    public void eliminar(Nodo tipoNodo){
+
+    public void eliminar(Nodo tipoNodo) {
         contenedor.remove(tipoNodo);
     }
+
     public Nodo getNodo(int posicion) {
         return contenedor.get(posicion);
     }
+
     //para que busque si hay una carpeto o archivo y lo devuelva
     public ArrayList<Nodo> getNodos() {
         return contenedor;
@@ -48,6 +65,5 @@ public class Carpeta extends Nodo {
         }
 
     }
-    
-    
+
 }
